3b1dd37d08dc5e6c5655437182a12900759ea556
ctlvie@gmail.com <ctlvie@gmail.com>
2018-05-07 22:12:29 +0800

    initial coding complete

--- tank_phy.v
+++ tank_phy.v
@@ -9,6 +9,7 @@ Modification History:
 Date		By			Version		Description
 ----------------------------------------------------------
 180505		QiiNn		0.5			Module interface definition
+180507		QiiNn		1.0			Initial coding completed (without add to object)
 ========================================================*/
 
 `timescale 1ns/1ns
@@ -27,6 +28,74 @@ module tank_phy
 	
 	//output the VGA data
 	output	reg	[11:0]	VGA_data
+	output 			VGA_en
 );
 
+// direction = upward 
+  always@(posedge clk)
+  begin
+    if (tank_state == 1'b1 && tank_dir == 2'b00)
+    begin
+      if (((VGA_xpos > x_rel_pos * 20 + 160 - 3)&&(VGA_xpos < x_rel_pos * 20 + 160 + 3))&&((VGA_ypos > y_rel_pos * 20 + 40 - 7)&&(VGA_ypos < y_rel_pos * 20 + 40)) ||
+      ((VGA_xpos > x_rel_pos * 20 + 160 - 7)&&(VGA_xpos < x_rel_pos *20 + 160 + 7))&&((VGA_ypos > y_rel_pos * 20 + 40)&&(VGA_ypos > y_rel_pos * 20 + 40 + 7))) 
+        begin
+          if (tank_ide == 1'b1)  VGA_data <= `BLUE;
+          else VGA_data <= `RED;
+          VGA_en <= 1'b1; 
+        end
+      else VGA_en <= 1'b0;
+    end
+  end
+  
+  
+  // direction = downward
+  always@(posedge clk)
+  begin
+    if (tank_state == 1'b1 && tank_dir == 2'b01)
+    begin
+      if (((VGA_xpos > x_rel_pos * 20 + 160 - 7)&&(VGA_xpos < x_rel_pos * 20 + 160 + 7))&&((VGA_ypos > y_rel_pos * 20 + 40 - 7)&&(VGA_ypos < y_rel_pos * 20 + 40)) ||
+          ((VGA_xpos > x_rel_pos * 20 + 160 - 3)&&(VGA_xpos < x_rel_pos *20 + 160 + 3))&&((VGA_ypos > y_rel_pos * 20 + 40)&&(VGA_ypos > y_rel_pos * 20 + 40 + 7))) 
+        begin
+          if (tank_ide == 1'b1)  VGA_data <= `BLUE;
+          else VGA_data <= `RED;
+          VGA_en <= 1'b1; 
+        end
+      else VGA_en <= 1'b0;
+    end
+  end
+  
+  
+  //direction = left
+  always@(posedge clk)
+  begin
+    if (tank_state == 1'b1 && tank_dir == 2'b10)
+    begin
+      if (((VGA_xpos > x_rel_pos * 20 + 160 - 7)&&(VGA_xpos < x_rel_pos * 20 + 160 ))&&((VGA_ypos > y_rel_pos * 20 + 40 - 3)&&(VGA_ypos < y_rel_pos * 20 + 40 + 3)) ||
+          ((VGA_xpos > x_rel_pos * 20 + 160 )&&(VGA_xpos < x_rel_pos *20 + 160 + 7))&&((VGA_ypos > y_rel_pos * 20 + 40 - 7)&&(VGA_ypos > y_rel_pos * 20 + 40 + 7))) 
+        begin
+          if (tank_ide == 1'b1)  VGA_data <= `BLUE;
+          else VGA_data <= `RED;
+          VGA_en <= 1'b1; 
+        end
+      else VGA_en <= 1'b0;
+    end
+  end
+  
+  
+  //direction = right
+  always@(posedge clk)
+  begin
+    if (tank_state == 1'b1 && tank_dir == 2'b11)
+    begin
+      if (((VGA_xpos > x_rel_pos * 20 + 160 - 7)&&(VGA_xpos < x_rel_pos * 20 + 160 ))&&((VGA_ypos > y_rel_pos * 20 + 40 - 7)&&(VGA_ypos < y_rel_pos * 20 + 40 + 7)) ||
+          ((VGA_xpos > x_rel_pos * 20 + 160 )&&(VGA_xpos < x_rel_pos *20 + 160 + 7))&&((VGA_ypos > y_rel_pos * 20 + 40 - 3)&&(VGA_ypos > y_rel_pos * 20 + 40 + 3))) 
+        begin
+          if (tank_ide == 1'b1)  VGA_data <= `BLUE;
+          else VGA_data <= `RED;
+          VGA_en <= 1'b1; 
+        end
+      else VGA_en <= 1'b0;
+    end
+  end
+  
 endmodule
